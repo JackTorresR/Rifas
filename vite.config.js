ccb3dd71ba31095974fc29bae73ab0892e52bc07
jacktorresr <jacktorresr@gmail.com>
2026-07-10 20:45:12 -0300

    Fix: Nome do repoitório para Rifas

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,4 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// Troque "rifa-beneficente" pelo nome exato do seu repositório no GitHub
-// caso o repositório tenha outro nome. Isso é necessário para o GitHub Pages
-// servir os arquivos estáticos no caminho correto.
-export default defineConfig({
-  plugins: [react()],
-  base: '/rifa-beneficente/',
-});
+export default defineConfig({ plugins: [react()], base: "/Rifas/" });
